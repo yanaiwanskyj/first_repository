0945bf981d3c17d41f88312371b58cf17dd30b8f
Yana Iwanskyj <yanaiwanskyj@gmail.com>
2026-03-04 12:56:57 +0000

    renaming variables

---
+++ cats.py
@@ -0,0 +1,11 @@
+## create a class called cats and create a cat
+
+class Cats:
+    def __init__(self, name):
+        self.name = name
+        self.colour = colour
+    
+    def meow(name, colour):
+        if colour = 'Black':
+            meow.
+
--- tax_calc.py
+++ tax_calc.py
@@ -1,29 +1,53 @@
 # this function calculates total tax on a random income between £1 and £100k
-# £12750 is tax free, the following 20k taxed at 20%, then the 
+# £12750 is tax free, the following 20k taxed at 20%, then the next amount taxed at 40%
 
-import random
+class Taxpayer:
+    def __init__(self, name, age, income):
+        self.name= name
+        self.__age = age ## double underscore creates priv variable
+        self.income = income
 
-num = list(range(1, 100001))
-income = random.choice(num)
+    def __taxable_incomes(self):
+        return self.income - 12750
 
-def income_tax_due(income):
-    taxable_income = income - 12750
-    if taxable_income >= 20000:
-        income_at_20_perc = 20000
-    else:
-        income_at_20_perc = taxable_income
-    if taxable_income >= 50000:
-        income_at_40_perc = 30000
-    else:
-        income_at_20_perc = taxable_income - 20000
+    def income_tax_due(self):
+        if self.__taxable_incomes() >= 20000:
+            income_at_20_perc = 20000
+        elif self.__taxable_incomes() > 0:
+            income_at_20_perc = self.__taxable_incomes()
+        else: 
+            income_at_20_perc = 0
+        if self.__taxable_incomes() - income_at_20_perc > 0:
+            income_at_40_perc = self.__taxable_incomes() - income_at_20_perc
+        else:
+            income_at_40_perc = 0
+        total_tax_paid = income_at_20_perc * 0.2 + income_at_40_perc*0.4
+        return total_tax_paid
+    
+    def tax_statement(self):
+        return(f"{self.name} owes {self.income_tax_due()} in tax for {self.income} earnings")
+    
+    def get_age(self): ### this allows you to take the private variable and call it... the underscores used earlier prevent make it private
+        return self.__age
+    
+    def get_taxable_incomes(self):
+        return self.__taxable_incomes
+
+## declaring an object of the class - taxpayer. object named person1.
+person1 = Taxpayer('Yana', 25, 50000)
+
+print(person1.get_age()) 
+print(person1.tax_statement())
+
+print(person1.get_taxable_incomes)
+
+print(person1)
+
+## inheritance- create a class based on another e.g. age check
 
 
-    income_at_50_perc = '7'
-    test= 0
-    if income_at_50_perc > 50000:
-        print('income too high for calculator')
-    else:
-        print('test')
-income_tax_due(income)
+## to rename symbol across code, ctrl right click and rename
+
+
 
 
